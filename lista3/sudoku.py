67ce466f05817986e1df6bac40dc09184d774ea6
Rafał Nowak <rafal.nowak@tooploox.com>
2019-04-14 18:48:23 +0200

    python3

--- lista3/sudoku.py
+++ lista3/sudoku.py
@@ -13,48 +13,54 @@ def all_different(Qs):
 def get_column(j):
     return [V(i,j) for i in range(9)] 
             
-def get_raw(i):
+def get_row(i):
     return [V(i,j) for j in range(9)] 
+
+def get_square(i, j):
+    return [V(q, p) for p in range(i*3, i*3+3) for q in range(j*3, j*3+3)]
+
+def squares():
+    return [all_different(get_square(i, j)) for i in range(3) for j in range(3)]
                         
 def horizontal():   
-    return [ all_different(get_raw(i)) for i in range(9)]
+    return [all_different(get_row(i)) for i in range(9)]
 
 def vertical():
     return [all_different(get_column(j)) for j in range(9)]
 
 def print_constraints(Cs, indent, d):
     position = indent
-    print (indent - 1) * ' ',
+    print((indent - 1) * ' ', end=' ')
     for c in Cs:
-        print c + ',',
+        print(c + ',', end=' ')
         position += len(c)
         if position > d:
             position = indent
-            print
-            print (indent - 1) * ' ',
+            print()
+            print((indent - 1) * ' ', end=' ')
 
       
 def sudoku(assigments):
     variables = [ V(i,j) for i in range(9) for j in range(9)]
     
-    print ':- use_module(library(clpfd)).'
-    print 'solve([' + ', '.join(variables) + ']) :- '
+    print(':- use_module(library(clpfd)).')
+    print('solve([' + ', '.join(variables) + ']) :- ')
     
-    
-    cs = domains(variables) + vertical() + horizontal() #TODO: too weak contraints, add something!
+    cs = domains(variables) + vertical() + horizontal() + squares()
+
     for i,j,val in assigments:
         cs.append( '%s #= %d' % (V(i,j), val) )
     
     print_constraints(cs, 4, 70),
-    print
-    print '    labeling([ff], [' +  ', '.join(variables) + ']).' 
-    print 
-    print ':- solve(X), write(X), nl.'       
+    print()
+    print('    labeling([ff], [' +  ', '.join(variables) + ']).') 
+    print() 
+    print(':- solve(X), write(X), nl.')       
 
 if __name__ == "__main__":
     raw = 0
     triples = []
-    
+      
     for x in sys.stdin:
         x = x.strip()
         if len(x) == 9:
